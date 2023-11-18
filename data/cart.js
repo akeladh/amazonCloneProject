f78544c707cb582d2631f9fab8ef6bd527d212f1
akeladh <akela.doho@gmail.com>
2023-11-17 19:02:53 -0800

    move addToCart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,22 @@
 export const cart = [];
+
+export function addToCart(productId){
+  let matchingItem;
+
+  //check if product is already in cart
+  cart.forEach((cartItem)=>{
+    if(productId === cartItem.productId){
+      matchingItem = cartItem;
+    }
+  });
+
+  //if already in cart, increase quantity if not push to cart array
+  if(matchingItem){
+    matchingItem.quantity++;
+  }else{
+    cart.push({
+      productId,
+      quantity: 1})
+  }
+
+}
